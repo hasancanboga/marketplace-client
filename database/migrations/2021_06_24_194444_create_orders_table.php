8d953f9bef057bac2fd569e9ea2fa527ac37bb07
John <hjohnboga@gmail.com>
2021-06-25 00:39:19 +0300

    created models and migrations

---
+++ database/migrations/2021_06_24_194444_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_method');
+            $table->string('shipping_method');
+            $table->foreignId('customer_id')->constrained();
+            $table->integer('company_id');
+            $table->string('type');
+            $table->foreignId('billing_address_id')->constrained('addresses');
+            $table->foreignId('shipping_address_id')->constrained('addresses');
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
